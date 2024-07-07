c53dc6f1866593b30424c69117a60b34292a92ab
iamyare <yarephotographer@gmail.com>
2024-07-07 11:23:34 -0600

    chore: Update Tailwind CSS configuration and dependencies

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -8,7 +8,7 @@ function App(): JSX.Element {
     <>
       <img alt="logo" className="logo" src={electronLogo} />
       <div className="creator">Powered by electron-vite</div>
-      <div className="text">
+      <div className="text ">
         Build an Electron app with <span className="react">React</span>
         &nbsp;and <span className="ts">TypeScript</span>
       </div>
