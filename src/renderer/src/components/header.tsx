8ef7a7400ed96d96e29d5e9e05c3ba9d507c9693
iamyare <yarephotographer@gmail.com>
2024-07-15 09:20:13 -0600

    feat: Update Content-Security-Policy to allow WebSocket connection to Supabase

--- src/renderer/src/components/header.tsx
+++ src/renderer/src/components/header.tsx
@@ -1,11 +1,19 @@
-import LogOut from './log-out'
+import ButtonGhost from '@renderer/components/ui/button-ghost'
 import { TooltipProvider } from './ui/tooltip'
+import { removeSession } from '@renderer/actions'
+import { useNavigate } from 'react-router-dom'
+import { LogOutIcon } from 'lucide-react'
 
 export default function HeaderNavbar() {
+  const navigate = useNavigate()
+  const handleClearSession = () => {
+    removeSession()
+    navigate('/auth/login')
+  }
   return (
     <header id="titlebar" className="fixed top-0 left-0 w-screen p-4">
       <TooltipProvider delayDuration={300}>
-        <LogOut />
+        <ButtonGhost icon={LogOutIcon} tooltip="Cerrar Sesion" onClick={handleClearSession} />
         {/* <button onClick={getInfoDeviceFunction}>Get Info Device</button> */}
       </TooltipProvider>
     </header>
--- src/renderer/src/components/ui/button-ghost.tsx
+++ src/renderer/src/components/ui/button-ghost.tsx
@@ -1,17 +1,21 @@
 import { Button } from './button'
 import { cn } from '@renderer/lib/utils'
-import React, { ReactElement, ComponentProps } from 'react'
+import { ComponentProps, ForwardRefExoticComponent, RefAttributes } from 'react'
 import { Tooltip, TooltipContent, TooltipTrigger } from '@renderer/components/ui/tooltip'
 
+// Definición ajustada para icon, permitiendo componentes que acepten className.
 interface ButtonGhostProps extends ComponentProps<typeof Button> {
   classNameIcon?: string
   tooltip: string
-  icon: ReactElement
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  icon: ForwardRefExoticComponent<Omit<any, 'ref'> & RefAttributes<SVGSVGElement>> & {
+    className?: string
+  }
 }
 
 export default function ButtonGhost({
   className,
-  icon,
+  icon: Icon,
   classNameIcon,
   tooltip,
   ...buttonProps
@@ -21,11 +25,14 @@ export default function ButtonGhost({
       <TooltipTrigger asChild>
         <Button
           {...buttonProps}
-          className={cn('hover:bg-transparent p-1 group relative', className)}
+          variant={'ghost'}
+          className={cn(
+            'hover:bg-transparent p-1 group relative transition-transform active:scale-95',
+            className
+          )}
         >
-          {React.cloneElement(icon, {
-            className: cn('size-4 opacity-80 group-hover:opacity-100', classNameIcon)
-          })}
+          {/* Uso de Icon con className, ahora es válido según la nueva definición de icon en ButtonGhostProps */}
+          <Icon className={cn('size-4 opacity-80 group-hover:opacity-100', classNameIcon)} />
           <div className="absolute size-2 bg-white transition-opacity duration-300 opacity-0 group-hover:opacity-50 blur-sm -z-[1]"></div>
           <div className="absolute size-3 bg-white transition-opacity duration-300 opacity-0 group-hover:opacity-50 blur-md -z-[1]"></div>
         </Button>
