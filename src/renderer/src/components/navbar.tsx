a25795b04ae906129a46a93be6be516584671fdc
iamyare <yarephotographer@gmail.com>
2024-07-27 15:06:56 -0600

    feat: Add settings and exit buttons to navbar

--- src/renderer/src/components/navbar.tsx
+++ src/renderer/src/components/navbar.tsx
@@ -2,7 +2,7 @@ import ButtonGhost from '@renderer/components/ui/button-ghost'
 import { TooltipProvider } from '@renderer/components/ui/tooltip'
 import { removeSession } from '@renderer/actions'
 import { useNavigate } from 'react-router-dom'
-import { LogOutIcon } from 'lucide-react'
+import { LogOutIcon, Settings, X } from 'lucide-react'
 
 export default function Navbar() {
   const navigate = useNavigate()
@@ -14,6 +14,18 @@ export default function Navbar() {
     <header id="titlebar" className="fixed top-0 left-0 w-screen p-4">
       <TooltipProvider delayDuration={300}>
         <ButtonGhost icon={LogOutIcon} tooltip="Cerrar Sesion" onClick={handleClearSession} />
+        <ButtonGhost
+          icon={Settings}
+          tooltip="Configuracion"
+          onClick={() => console.log('Abre modal config')}
+        />
+        <ButtonGhost
+          icon={X}
+          tooltip="Salir"
+          className="fixed top-0 right-2"
+          onClick={() => console.log('Cerrar app')}
+        />
+
         {/* <button onClick={getInfoDeviceFunction}>Get Info Device</button> */}
       </TooltipProvider>
     </header>
