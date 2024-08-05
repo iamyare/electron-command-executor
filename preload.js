c8e84fd445faffcb9006267b173610c30e91d8b1
iamyare <yarephotographer@gmail.com>
2024-08-05 13:58:37 -0600

    chore: Remove unnecessary files and dependencies

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  sendCommand: (command) => ipcRenderer.send('execute-command', command),
-  onCommandResult: (callback) => ipcRenderer.on('command-result', callback),
-});
